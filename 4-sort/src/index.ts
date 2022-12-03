ad31a43810cc1f4b4e407593115ee0e120146122
AHMAD-KHATIB <ahmadkhatib923@gmail.com>
2022-12-03 16:48:58 +0300

    string sorting impelentation

--- 4-sort/src/index.ts
+++ 4-sort/src/index.ts
@@ -1,8 +1,14 @@
-import { Sorter } from "./Sorter";
-import { NumbersCollection } from "./NumbersCollection";
+import { Sorter } from './Sorter'
+import { NumbersCollection } from './NumbersCollection'
+import { CharactersCollection } from './CharactersCollection'
 
-const numbersCollection = new NumbersCollection([1000090, 3, -5, 0]);
-const sorter = new Sorter(numbersCollection);
-sorter.sort();
+// const numbersCollection = new NumbersCollection([1000090, 3, -5, 0]);
+// const sorter = new Sorter(numbersCollection);
+// sorter.sort();
 
-console.log(numbersCollection);
+// console.log(numbersCollection);
+
+const charactersCollection = new CharactersCollection('Xaaaby')
+const sorter = new Sorter(charactersCollection)
+sorter.sort()
+console.log(charactersCollection.data)
