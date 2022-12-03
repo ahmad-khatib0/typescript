cff55020760e40838d77575115f387558addf358
AHMAD-KHATIB <ahmadkhatib923@gmail.com>
2022-12-03 09:48:04 +0300

    impelementing a general sorter

---
+++ 4-sort/build/index.js
@@ -0,0 +1,21 @@
+"use strict";
+class Sorter {
+    constructor(collection) {
+        this.collection = collection;
+    }
+    sort() {
+        const { length } = this.collection;
+        for (let i = 0; i < length; i++) {
+            for (let j = 0; j < length - i - 1; j++) {
+                if (this.collection[j] > this.collection[j + 1]) {
+                    const leftHand = this.collection[j];
+                    this.collection[j] = this.collection[j + 1]; // swap right side to left
+                    this.collection[j + 1] = leftHand; // swap the left side to right
+                }
+            }
+        }
+    }
+}
+const sorter = new Sorter([10, 3, -5, 0]);
+sorter.sort();
+console.log(sorter.collection);
