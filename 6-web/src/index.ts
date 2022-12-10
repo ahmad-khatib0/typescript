ea8696067530c859dc8f3d147352a69009c834e8
AHMAD-KHATIB <ahmadkhatib923@gmail.com>
2022-12-11 01:21:40 +0300

    object keys as types

--- 6-web/src/index.ts
+++ 6-web/src/index.ts
@@ -4,5 +4,3 @@ import { User } from './models/User'
 // user.set({ name: 'new name' })
 // user.set({ age: 44 })
 const user = new User({ name: 'new record ', age: 444 })
-user.events.on('change', () => console.log('change!'))
-user.events.trigger('change')
--- 6-web/src/models/Attributes.ts
+++ 6-web/src/models/Attributes.ts
@@ -1,8 +1,12 @@
 export class Attributes<T> {
   constructor(private data: T) {}
-
-  get(propName: string): string | number {
-    return this.data[propName]
+  // like T, K is alos a convention name, e,g can to be KeyOfObject
+  // so here  <K extends keyof T>  e.g UserProps has name, id , age, so the K can only be one of those keys
+  // so now THIS HERE IS LIKE  e.g being the id string as a type , so here the object's keys are the TYPES
+  // and the return type which is T[K]  is like for example user['id'],
+  // so return one of the needed keys that are existed inside this generic T
+  get<K extends keyof T>(key: K): T[K] {
+    return this.data[key]
   }
 
   set(update: T): void {
