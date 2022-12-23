aa1617056018a1c25a0aa72e973e34216a38f391
AHMAD-KHATIB <ahmadkhatib923@gmail.com>
2022-12-24 01:25:16 +0300

    protected routes

--- 7-ts-with-express/src/routes/loginRoutes.ts
+++ 7-ts-with-express/src/routes/loginRoutes.ts
@@ -1,7 +1,16 @@
-import { Router, Request, Response } from 'express'
+import { Router, Request, Response, NextFunction } from 'express'
 
 interface RequestWithBody extends Request {
   body: { [key: string]: string | undefined }
+}
+
+function requireAuth(req: Request, res: Response, next: NextFunction): void {
+  if (req.session && req.session.isLoggedIn) {
+    next()
+    return
+  }
+  res.status(403)
+  res.send('not permitted')
 }
 
 const router = Router()
@@ -55,6 +64,10 @@ router.get('/', (req: Request, res: Response) => {
 router.get('/logout', (req: Request, res: Response) => {
   req.session = undefined
   res.redirect('/')
+})
+
+router.get('/protected', requireAuth, (req: Request, res: Response) => {
+  res.send('welcome to protected route, logged in user')
 })
 
 export { router }
