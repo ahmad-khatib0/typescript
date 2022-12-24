ec30c979fdcaef66657dc97a75af6f55ee060bb3
AHMAD-KHATIB <ahmadkhatib923@gmail.com>
2022-12-24 16:43:21 +0300

    basics of metadata

---
+++ 2-types/metadata.ts
@@ -0,0 +1,19 @@
+import 'reflect-metadata'
+
+// const plane = {
+//   color: 'red',
+// }
+
+// Reflect.defineMetadata('note', 'hey you!', plane)
+// Reflect.defineMetadata('height', 10, plane)
+// Reflect.defineMetadata('anotherNote', 'this is a metadata info for a property', plane, 'color')
+
+// const note = Reflect.getMetadata('note', plane)
+// const height = Reflect.getMetadata('height', plane)
+// const color = Reflect.getMetadata('anotherNote', plane, 'color')
+
+// console.log(plane) //{ color: 'red' }
+
+// console.log(note) // hey you!
+// console.log(height) // 10
+// console.log(color) // this is a metadata info for a property
