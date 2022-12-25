39712ad7bcaa157951ca0705efb6bcaa931b6f7e
AHMAD-KHATIB <ahmadkhatib923@gmail.com>
2022-12-25 23:56:55 +0300

    refectoring

---
+++ 7-ts-with-express/src/controllers/RootController.ts
@@ -0,0 +1,40 @@
+import { NextFunction, Request, Response } from 'express'
+import { get, controller, use } from './decorators'
+
+function requireAuth(req: Request, res: Response, next: NextFunction): void {
+  if (req.session && req.session.isLoggedIn) {
+    next()
+    return
+  }
+  res.status(403)
+  res.send('not permitted')
+}
+
+@controller('') // prevent having tow slashes
+export class RootController {
+  @get('/')
+  getRoot(req: Request, res: Response) {
+    // this left check is a type guard, in order to access isLoggedIn in the right check
+    if (req.session && req.session.isLoggedIn) {
+      res.send(`
+       <div> 
+           <div> You are logged in </div> 
+           <a  href="/auth/logout"> Logout </a>  
+       </div> 
+    `)
+    } else {
+      res.send(`
+    <div> 
+         <div> You are not logged in </div> 
+         <a href="/auth/login"> Login </a> 
+    </div> 
+   `)
+    }
+  }
+
+  @get('/protected')
+  @use(requireAuth)
+  getProtected(req: Request, res: Response) {
+    res.send('welcome to protected route, logged in user')
+  }
+}
