7442fa9ad0eae33280b80bf08639cba6509f7305
AHMAD-KHATIB <ahmadkhatib923@gmail.com>
2022-12-25 21:06:10 +0300

    restructure the app

--- 7-ts-with-express/src/index.ts
+++ 7-ts-with-express/src/index.ts
@@ -1,12 +1,15 @@
 import bodyParser from 'body-parser'
 import cookieSession from 'cookie-session'
-import express, { Request, Response } from 'express'
+import express from 'express'
 import { router } from './routes/loginRoutes'
+import './controllers/LoginController'
+import { AppRouter } from './AppRouter'
 
 const app = express()
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cookieSession({ keys: ['asldfdadfsfd'] }))
 app.use(router)
+app.use(AppRouter.getInstance())
 
 app.listen(3000, () => console.log('listining on port 3000'))
