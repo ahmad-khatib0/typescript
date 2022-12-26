40346e8d8768394853e84abda4b9f686344b5eec
AHMAD-KHATIB <ahmadkhatib923@gmail.com>
2022-12-27 02:55:09 +0300

    generic for a dispatch

--- 8-ts-react-redux/src/store/actions/index.ts
+++ 8-ts-react-redux/src/store/actions/index.ts
@@ -8,14 +8,19 @@ interface Todo {
   completed: boolean
 }
 
+interface FetchTodosAction {
+  type: ActionTypes.fetchTodos
+  payload: Todo[]
+}
+
 const url = 'https://jsonplaceholder.typicode.com/todos'
 export const fetchTodos = () => {
   return async (dispatch: Dispatch) => {
     const res = await axios.get<Todo[]>(url)
 
-    dispatch({
+    dispatch<FetchTodosAction>({
       type: ActionTypes.fetchTodos,
-      paylaod: res.data,
+      payload: res.data,
     })
   }
 }
