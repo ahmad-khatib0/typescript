e4a75d6135b73154a6d007f88fde04075ed02898
AHMAD-KHATIB <ahmadkhatib923@gmail.com>
2022-12-27 01:42:23 +0300

    app props and state

--- 8-ts-react-redux/src/index.tsx
+++ 8-ts-react-redux/src/index.tsx
@@ -1,10 +1,41 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-export class App extends React.Component {
+interface AppProps {
+  color?: string
+}
+interface AppState {
+  counter?: number
+}
+
+export class App extends React.Component<AppProps, AppState> {
+  // state = { counter: 0 }  // this is shorter, you don't need the constructor and AppState with it
+  // NOTE: you need to chose an option only, ethier the the constructor approach or this shorter one
+
+  constructor(props: AppProps) {
+    super(props)
+    this.state = { counter: 0 }
+  }
+
+  onIncrement = (): void => {
+    this.setState({ counter: this.state.counter! + 1 })
+  }
+  onDecrement = (): void => {
+    this.setState({ counter: this.state.counter! - 1 })
+  }
+
   render() {
-    return <div> hi there </div>
+    return (
+      <div>
+        {this.props.color}
+        <br />
+        <div>bar</div>
+        <button onClick={this.onIncrement}>Increment </button>
+        <button onClick={this.onDecrement}> Decrement </button>
+        {this.state.counter}
+      </div>
+    )
   }
 }
 
-ReactDOM.render(<App />, document.querySelector('#root'))
+ReactDOM.render(<App color="red" />, document.querySelector('#root'))
