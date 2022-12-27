802c0982c7f592c5e4d5a228a2c2df7770b4f012
AHMAD-KHATIB <ahmadkhatib923@gmail.com>
2022-12-27 03:15:57 +0300

    combineReducers generic interface as a great feature that make a return type  validation

--- 8-ts-react-redux/src/store/reducers/index.ts
+++ 8-ts-react-redux/src/store/reducers/index.ts
@@ -1,5 +1,14 @@
 import { combineReducers } from 'redux'
+import { Todo } from '../actions'
+import { todosReducer } from './todos'
 
-export const reducers = combineReducers({
-  counter: () => 1,
+export interface StoreState {
+  todos: Todo[]
+}
+
+export const reducers = combineReducers<StoreState>({
+  // by Providing this optional generic annotation, this will not ONLY make sure that the keys are are
+  // matching, but for example by using this generic , ts will make sure that todosReducer returns
+  // the correct type (which is an array of Todo in this case )
+  todos: todosReducer,
 })
